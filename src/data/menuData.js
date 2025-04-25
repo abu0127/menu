b2f16d655c6b32ee33df0d62caff3ded0b8c8e3a
abu0127 <sr20010127@gmail.com>
2025-04-25 17:45:45 +0300

    Auto deploy commit

--- src/data/menuData.js
+++ src/data/menuData.js
@@ -34,6 +34,73 @@ export const menu = [
      
       
     ]
+  },
+  {
+    category: "Ichimliklar",
+    image: drinks,
+    items: [
+      {
+        id: 1,
+        name: "Coca-Cola",
+        originalPrice: 15000, // Original price before discount
+        discount: 0, //  discount
+        image: "../images/cola-450.png",
+        isOnSale: true // Flag to indicate item is on sale
+      },
+      {
+        id: 2,
+        name: "Pepsi",
+        originalPrice: 13000,
+        discount: 10,
+        image: "../images/pepsi.jpg",
+        isOnSale: true
+      },
+      {
+        id: 3,
+        name: "Red Bull",
+        originalPrice: 20000,
+        discount: 0,
+        image: "../images/redbul.jpg",
+        isOnSale: false,
+      }
+      ,
+     
+      
+    ]
+  }
+  ,
+  {
+    category: "Ichimliklar",
+    image: drinks,
+    items: [
+      {
+        id: 1,
+        name: "Coca-Cola",
+        originalPrice: 15000, // Original price before discount
+        discount: 0, //  discount
+        image: "../images/cola-450.png",
+        isOnSale: true // Flag to indicate item is on sale
+      },
+      {
+        id: 2,
+        name: "Pepsi",
+        originalPrice: 13000,
+        discount: 10,
+        image: "../images/pepsi.jpg",
+        isOnSale: true
+      },
+      {
+        id: 3,
+        name: "Red Bull",
+        originalPrice: 20000,
+        discount: 0,
+        image: "../images/redbul.jpg",
+        isOnSale: false,
+      }
+      ,
+     
+      
+    ]
   }
   ,
   {
