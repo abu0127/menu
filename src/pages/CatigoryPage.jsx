9a2a4c65f3520e1bf670aed63c9357393326c349
abu0127 <sr20010127@gmail.com>
2025-04-25 16:04:34 +0300

    Auto deploy commit

--- src/pages/CatigoryPage.jsx
+++ src/pages/CatigoryPage.jsx
@@ -1,33 +1,50 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useLoaderData } from "react-router-dom";
 import ProductCard from "../components/ProductCard";
+import ProductCardLoader from "../components/ProductCardLoader"; // Yangi loader komponenti
 import ProductModal from "../components/ProductModal";
 import "../styles/products.css";
 
 const CategoryPage = () => {
   const category = useLoaderData();
   const [selectedProduct, setSelectedProduct] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Ma'lumotlar yuklanganligini simulyatsiya qilish
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+    
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <div className="category-container">
       <div className="products-grid">
-        {category.items.map(item => (
-          <div
-            key={item.id}
-            onClick={() => item.isOnSale && setSelectedProduct(item)}
-          >
-            <ProductCard
-              product={item}
-              name={item.name}
-              price={item.price}
-              image={item.image}
-              description={item.description}
-              originalPrice={item.originalPrice}
-              discount={item.discount}
-              isOnSale={item.isOnSale}
-            />
-          </div>
-        ))}
+        {isLoading ? (
+          // Yuklanayotganda loader ko'rsatamiz
+          <ProductCardLoader count={6} />
+        ) : (
+          // Yuklanganidan keyin mahsulotlarni ko'rsatamiz
+          category.items.map(item => (
+            <div
+              key={item.id}
+              onClick={() => item.isOnSale && setSelectedProduct(item)}
+            >
+              <ProductCard
+                product={item}
+                name={item.name}
+                price={item.price}
+                image={item.image}
+                description={item.description}
+                originalPrice={item.originalPrice}
+                discount={item.discount}
+                isOnSale={item.isOnSale}
+              />
+            </div>
+          ))
+        )}
       </div>
 
       {/* Modal oynasi */}
