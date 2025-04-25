3e64a69ffaf742ca28ea31513ed65093f83a79fe
abu0127 <sr20010127@gmail.com>
2025-04-25 16:20:52 +0300

    Auto deploy commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,34 +1,53 @@
 import { Link } from "react-router-dom";
 import { menu } from "../data/menuData";
 import Animation from "../components/Animation";
+import ProductCardLoader from "../components/ProductCardLoader"; // ProductCardLoader import qilamiz
+import { useState, useEffect } from "react";
 import "../styles/home.css";
 
-
 const Home = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+    
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="home-page">
-      <div className="category-list">
-        {menu.map((cat, i) => (
-          <Animation key={i}>
-            <Link
-              to={`/category/${cat.category.toLowerCase()}`}
-              className="category-link"
-            >
-              <div className="category-card">
-                {/* Kategoriya rasmi */}
-                <div className="category-title">{cat.category}</div>
-                <img 
-                  loading="lazy"
-                  src={cat.image} 
-                  alt={cat.category} 
-                  className="category-image"
-                />
-               
-              </div>
-            </Link>
-          </Animation>
-        ))}
-      </div>
+      {loading ? (
+        <div className="category-list">
+          {[...Array(4)].map((_, i) => (
+            <div key={i} className="category-link-placeholder">
+              <ProductCardLoader />
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div className="category-list">
+          {menu.map((cat, i) => (
+            <Animation key={i}>
+              <Link
+                to={`/category/${cat.category.toLowerCase()}`}
+                className="category-link"
+              >
+                <div className="category-card">
+                  <div className="category-title">{cat.category}</div>
+                  <img 
+                    loading="lazy"
+                    src={cat.image} 
+                    alt={cat.category} 
+                    className="category-image"
+                  />
+                </div>
+              </Link>
+            </Animation>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
